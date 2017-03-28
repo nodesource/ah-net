a71903051479b1976a66881752727357580ae672
Thorsten Lorenz <thlorenz@gmx.de>
2017-03-28 11:35:29 +0200

    docs: cleanup and adding documentation

--- ah-net.js
+++ ah-net.js
@@ -20,9 +20,6 @@ const types = new Set([
 ])
 
 function isnetType(type) {
-  // TODO: when implementing further examples make sure to consider TickObjects
-  // as they maybe more important in those cases.
-  // return true
   return types.has(type)
 }
 
@@ -35,6 +32,41 @@ const defaultStackCapturer = new StackCapturer({
 })
 
 class NetworkActivityCollector extends ActivityCollector {
+  /**
+   * Instantiates a NetworkActivityCollector.
+   *
+   * Most of the actual processing of resources is performed by th @see
+   * NetworkResourceProcessor.
+   *
+   * Extends [ActivityCollector](https://github.com/nodesource/ah-collector) and thus
+   * exposes the same [public
+   * API](https://github.com/nodesource/ah-collector#api) with added
+   * functionality.
+   *
+   * @param {Array.<number>} $0.start the start time of the process, i.e. the result of `process.hrtime()`
+   * @param {StackCapturer} [$0.stackCapturer=StackCapturer] [see ah-stack-capturer](https://github.com/nodesource/ah-stack-capturer) which
+   * configures how and when stacks traces are captured and processed.
+   *
+   * By default a StackCapturer is used that captures stacks for all events for
+   * file system related types: `FSREQWRAP`, `FSREQUESTWRAP` and some others like
+   * `TickObject`s that also are related, i.e. if they contain information related
+   * to streams.
+   *
+   * @param {number} [$0.bufferLength=0] determines how many elements of Buffers are
+   * captured. By default not Buffer data is captured.
+   *
+   * @param {number} [$0.stringLength=0] determines how much of each string is
+   * captured. By default no string data is captured.
+   *
+   * @param {boolean} [$0.captureArguments=false] if `true` arguments of callbacks
+   * are captured when they are processed.
+   *
+   * @param {boolean} [$0.captureSource=false] if `true` the source code of callbacks
+   * is captured when they are processed.
+   *
+   * @constructor
+   * @name NetworkActivityCollector
+   */
   constructor({
       start
     , stackCapturer = defaultStackCapturer
@@ -76,7 +108,6 @@ class NetworkActivityCollector extends ActivityCollector {
   // @override
   _after(uid) {
     const h = super._after(uid)
-    // if (h.type === 'HTTPPARSER') print(h)
     this._resourceProcessor
       .cleanupResource(uid, this.activities, { collectFunctionInfo: true })
     return h
